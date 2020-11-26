d9ad78ed80c305f8d81daa9aedd72706f3d41e13
hjjdpark <hyojinjdpark@HyojinJDParks-MacBook-Pro.local>
2020-11-26 23:31:02 +0900

    add step2

--- AlphabetGame/AlphabetGame.py
+++ AlphabetGame/AlphabetGame.py
@@ -4,6 +4,7 @@ from random import randint
 import array as arr
 # First, generate random alphabets
 ## step 1. Make two arrays of alphabets, one is for consonants, the other is for vowels
+## step 2. choose two words from vowels, choose five words from consonants and show them inorderly.
 vowels=arr.array('u', ['a' ,'e' ,'i' ,'o' ,'u' ,'y'])
 consonants=arr.array('u',['b','c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'])
 
@@ -14,6 +15,25 @@ for index in range(len(vowels)):
 for index in range(len(consonants)):
     print(consonants[index])
 '''
+random_vowels=arr.array('u',[])
+for _ in range(2):
+    index=randint(0,len(vowels)-1)
+    random_vowels.append(vowels[index])
+
+random_consonants=arr.array('u',[])
+for _ in range(5):
+    index=randint(0,len(consonants)-1)
+    random_consonants.append(consonants[index])
+
+''' to check result of step 2.
+print(random_vowels)
+print(random_consonants)
+'''
+for index in range(2):
+    print(random_vowels[index] , end=" ")
+for index in range(5):
+    print(random_consonants[index],end=" ")
+print() #to add new line
 
 #Second, Input your word , if user inputs "quit!", then end this program
 
