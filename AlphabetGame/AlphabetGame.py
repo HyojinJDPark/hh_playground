0b61cc4e0dc5bf605542602867db9d6f16a68e2c
hjjdpark <hyojinjdpark@HyojinJDParks-MacBook-Pro.local>
2020-11-26 22:25:32 +0900

    Add comment for basic structure

--- AlphabetGame/AlphabetGame.py
+++ AlphabetGame/AlphabetGame.py
@@ -1,1 +1,9 @@
 print("Hello world")
+
+from random import randint
+
+# First, generate random alphabets
+
+#Second, Input your word , if user inputs "quit!", then end this program
+
+#Third, Figure out wheter your input word is existed in Dictionary or not
